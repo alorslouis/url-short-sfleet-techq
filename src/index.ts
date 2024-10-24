13d0bfce00874666333fdc783ca8bdc321612087
alorslouis <alorslouis@users.noreply.github.com>
2024-10-24 11:20:59 -0400

    init routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import { Hono } from 'hono'
+import { Hono } from "hono"
+import { z } from "zod"
+
+const shortURlRecord = z.object({
+  id: z.number(),
+  originalUrl: z.string().url(),
+  shortUrl: z.string(),
+  reqOriginatingIp: z.string().ip(),
+  createdAt: z.date(),
+})
+
+export type ShortURlRecord = z.infer<typeof shortURlRecord>
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/", (c) => {
+  return c.text("Hello Hono!")
 })
+
+app.get("/r/:short", async (c) => {
+
+  const shortUrlParam = c.req.param("short")
+
+  return c.text(shortUrlParam)
+})
+
+app.post("/create", (c) => {
+  return c.text("Hello Hono!")
+})
+
+
 
 export default app
